ceef4adbd16be488a74e55283a3d3b5b402790ad
IreneArgabio <76997324+IreneArgabio@users.noreply.github.com>
2021-02-25 21:08:11 +0700

    Update and rename TODAY.html to index.php

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
  <frame frameborder="0", scrolling="no", Noresize="noresize", src="C:\Users\Home\Documents\TLE 3RD PT\PT FRAME1.html">
  <frame frameborder="0", scrolling="no", Noresize="noresize", src="C:\Users\Home\Documents\TLE 3RD PT\PT FRAME2.html">
  <frame frameborder="0", scrolling="yes", Noresize="noresize", src="C:\Users\Home\Documents\TLE 3RD PT\PT FRAME3.html">
-<frameset/>
-<html/>
+</frameset>
+</html>
